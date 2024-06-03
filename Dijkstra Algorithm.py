df540660ffddd8361401af60a3cc532afd70a58c
Poornima102004-M <149482191+Poornima102004-M@users.noreply.github.com>
2024-06-03 19:25:08 +0530

    Add files via upload

---
+++ Dijkstra Algorithm.py
@@ -0,0 +1,31 @@
+import heapq
+
+def dijkstra(graph, start):
+    distances = {vertex: float('infinity') for vertex in graph}
+    distances[start] = 0
+    priority_queue = [(0, start)]
+
+    while priority_queue:
+        current_distance, current_vertex = heapq.heappop(priority_queue)
+
+        if current_distance > distances[current_vertex]:
+            continue
+
+        for neighbor, weight in graph[current_vertex].items():
+            distance = current_distance + weight
+
+            if distance < distances[neighbor]:
+                distances[neighbor] = distance
+                heapq.heappush(priority_queue, (distance, neighbor))
+
+    return distances
+
+graph = {
+    'A': {'B': 5, 'C': 3},
+    'B': {'A': 5, 'C': 2, 'D': 1},
+    'C': {'A': 3, 'B': 2, 'D': 3},
+    'D': {'B': 1, 'C': 3}
+}
+
+start_vertex = 'A'
+print(dijkstra(graph, start_vertex))
